6068fd649e6c941a1b4d30ddc5f3cc261f5c18ec
Kolapo Oladotun <oladotunkolapo@gmail.com>
2024-09-07 17:07:22 +0100

    Update settings.py

--- shoply/settings.py
+++ shoply/settings.py
@@ -209,7 +209,7 @@ AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
 
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
