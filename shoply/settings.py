db702b29fac0fcf1e5065c218e2ff9c45eae59c4
Oladotun Kolapo <oladotunkolapo@gmail.com>
2025-05-25 20:29:00 +0100

    Update settings.py

--- shoply/settings.py
+++ shoply/settings.py
@@ -96,25 +96,25 @@ AUTH_USER_MODEL = "users.CustomUser"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': 'localhost',
-            'PORT': 5432,
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            # Replace this value with your local database's connection string.
-            default= os.getenv('DATABASE_URL'),
-            conn_max_age=600
-        )
-    }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': os.getenv('DB_NAME'),
+#             'USER': os.getenv('DB_USER'),
+#             'PASSWORD': os.getenv('DB_PASSWORD'),
+#             'HOST': 'localhost',
+#             'PORT': 5432,
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default= os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
